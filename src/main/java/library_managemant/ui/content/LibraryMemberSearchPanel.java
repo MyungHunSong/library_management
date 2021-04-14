b2ef1f6c46351c7e49097d4bd754285223394a4e
MyungHunSong <change1677@naver.com>
2021-04-14 09:21:49 +0900

    4/13

--- src/main/java/library_managemant/ui/content/LibraryMemberSearchPanel.java
+++ src/main/java/library_managemant/ui/content/LibraryMemberSearchPanel.java
@@ -13,14 +13,17 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
+import library_managemant.dto.MemberInfo;
 import library_managemant.service.MemberService;
 import library_managemant.ui.main.LibraryManagemantMain;
 
 public class LibraryMemberSearchPanel extends JPanel implements ActionListener {
 	private JTextField textField;
 	private JButton btnSearch;
+	
 	private JComboBox cmb1;
 	private String[] memInfo = {"회원 번호","회원 이름"};
+	
 	private LibraryManagemantMain libMan;
 	private MemberService service;
 	
@@ -37,6 +40,22 @@ public class LibraryMemberSearchPanel extends JPanel implements ActionListener {
 		}
 		
 	}
+	
+	// 요 기능 사용
+	public MemberInfo getMemberInfo() {
+		int memberNo = Integer.parseInt(textField.getText().trim());
+		String memberName = textField.getText().trim();
+		
+		return new MemberInfo(memberNo, memberName);
+	}
+	public void setMemberInfo(MemberInfo memberInfo) {
+		textField.setText(memberInfo.getMemberNo()+"");
+		textField.setText(memberInfo.getName());
+	}
+	public void clearTf() {
+		textField.setText("");
+	}
+	
 	
 	private void initialize() {
 		setLayout(new GridLayout(0, 1, 0, 0));
@@ -66,19 +85,23 @@ public class LibraryMemberSearchPanel extends JPanel implements ActionListener {
 		
 	}
 
-	public void actionPerformed(ActionEvent arg0) {
-		if (arg0.getSource() == textField) {
-			textFieldActionPerformed(arg0);
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == textField) {
+			textFieldActionPerformed(e);
 		}
-		if (arg0.getSource() == btnSearch) {
-			btnNewButtonActionPerformed(arg0);
+		if (e.getSource() == btnSearch) {
+			btnNewButtonActionPerformed(e);
+			if(cmb1.equals("회원번호")&& textField.equals("회원번호")) {
+				
+			}
 		}
 	}
-	protected void btnNewButtonActionPerformed(ActionEvent arg0) {
+	protected void btnNewButtonActionPerformed(ActionEvent e) {
 		JOptionPane.showMessageDialog(null, "cmb1");
 		
 	}
-	protected void textFieldActionPerformed(ActionEvent arg0) {
-		
+	protected void textFieldActionPerformed(ActionEvent e) {
+		 
+		 
 	}
 }
